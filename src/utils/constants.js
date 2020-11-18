45ed92d43bd7090f5b668e888b96f13332f35468
Daniel Paz <danielpaz601@gmail.com>
2020-11-18 21:27:04 +0200

    Added gh-pages

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,10 +3,10 @@ import iceCreamImage from '../assets/images/ice_cream.svg';
 
 export const ROUND_TIME = 60;
 export const MOVEMENT_REWARD = 10;
-// export const ROWS = 17;
-// export const COLS = 33;
-export const ROWS = 5;
-export const COLS = 3;
+export const ROWS = 17;
+export const COLS = 33;
+// export const ROWS = 5;
+// export const COLS = 3;
 
 export const KEYBOARD = {
   Enter: 'Enter',
