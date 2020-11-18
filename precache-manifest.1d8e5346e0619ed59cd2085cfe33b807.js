14fa431fb465e8b22342175b20e938121aa996ae
Daniel Paz <danielpaz601@gmail.com>
2020-11-18 21:27:55 +0200

    Updates

---
+++ precache-manifest.1d8e5346e0619ed59cd2085cfe33b807.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0448162b6abc78a48e001156fcfdeba",
+    "url": "/Maze/index.html"
+  },
+  {
+    "revision": "80b3f78fa64935c461d8",
+    "url": "/Maze/static/css/main.1856df13.chunk.css"
+  },
+  {
+    "revision": "a382eeb103c7196d80a6",
+    "url": "/Maze/static/js/2.ddcc9dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Maze/static/js/2.ddcc9dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b3f78fa64935c461d8",
+    "url": "/Maze/static/js/main.0bfc5651.chunk.js"
+  },
+  {
+    "revision": "cc48569096b1271dc339",
+    "url": "/Maze/static/js/runtime-main.733a612d.js"
+  },
+  {
+    "revision": "c760f5dc8f488d601a6bc6f8c12c75a9",
+    "url": "/Maze/static/media/ice_cream.c760f5dc.svg"
+  },
+  {
+    "revision": "18abc72225d23f9393a4bddfe0c9351c",
+    "url": "/Maze/static/media/joystix monospace.18abc722.ttf"
+  },
+  {
+    "revision": "3931d9579054690abf199c52e770e837",
+    "url": "/Maze/static/media/level_end.3931d957.mp3"
+  },
+  {
+    "revision": "f342226d7e050557d928f185b16b4906",
+    "url": "/Maze/static/media/logo.f342226d.svg"
+  },
+  {
+    "revision": "5bf858eeb94941d67f1a71157f9cf6a7",
+    "url": "/Maze/static/media/lollipop.5bf858ee.svg"
+  },
+  {
+    "revision": "12c141ff64f3ffd11704f08e0871185a",
+    "url": "/Maze/static/media/maze.12c141ff.mp3"
+  }
+]);
